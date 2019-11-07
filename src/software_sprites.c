296572cc6e3d561936977fb7cc3be9ca6efccf7f
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2019-11-07 23:09:05 +0100

    Clear screen OK

--- src/software_sprites.c
+++ src/software_sprites.c
@@ -5,11 +5,20 @@
 #include "software_sprites.h"
 
 #define SCREEN_START 0x2000
-#define SCREEN_END (0x2000+8000)
+#define SCREEN_END (SCREEN_START+8000)
+#define LUMA_START 0x1800
+#define LUMA_END (LUMA_START+1000)
+#define CHROMA_START 0x1C00
+#define CHROMA_END (CHROMA_START+1000)
+
 
 void bitmap_mode(void)
 {
-    POKE(0xFF12,PEEK(0xFF12)|32);
+    POKE(0xFF06,PEEK(0xFF06)|32);
+    POKE(0xFF12,PEEK(0xFF12)|8);
+    POKE(0xFF12,PEEK(0xFF12)&(0xFF-16-32));    
+    POKE(0xFF14,PEEK(0xFF14)|(8+16));
+    POKE(0xFF14,PEEK(0xFF14)&(0xFF-32-64-128));
 }
 
 void clear_screen(void)
@@ -17,6 +26,14 @@ void clear_screen(void)
     unsigned short i;
     
     for(i=SCREEN_START;i<SCREEN_END;++i)
+    {
+        POKE(i,0);
+    }
+    for(i=LUMA_START;i<LUMA_END;++i)
+    {
+        POKE(i,0);
+    }
+    for(i=CHROMA_START;i<CHROMA_END;++i)
     {
         POKE(i,0);
     }
